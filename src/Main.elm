d764cb935830c7349d578b88ae5e40889558da54
MitchellNorthern <s720414@sbuniv.edu>
2020-01-14 22:49:09 -0600

    Initial progress on Todos app

--- src/Main.elm
+++ src/Main.elm
@@ -1,20 +1,33 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Html.Keyed exposing (..)
+
+
+
+---- TYPES ----
+
+
+type alias Todo =
+    { name : String
+    , completed : Bool
+    }
+
 
 
 ---- MODEL ----
 
 
 type alias Model =
-    {}
+    List Todo
 
 
-init : ( Model, Cmd Msg )
+init : Model
 init =
-    ( {}, Cmd.none )
+    []
 
 
 
@@ -36,10 +49,38 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+    div [ class "grid-container" ]
+        [ div [ class "grid-sidebar" ] []
+        , div [ class "grid-body" ]
+            [ h1 [ class "body-title" ] [ text "Todo App in Elm" ]
+            , div [ class "body-content" ]
+                [ div [ class "todos-container" ]
+                    [ div [ class "todos-heading" ]
+                        [ text ""
+                        , div [ class "todos-input__wrapper" ]
+                            [ input [ class "todos-input__input", placeholder "Clean the car" ]
+                                []
+                            ]
+                        ]
+                    , renderAllTodos model
+                    ]
+                ]
+            ]
+        , div [ class "grid-sidebar" ] [ text "sidebar" ]
         ]
+
+
+renderTodo : Todo -> Html Msg
+renderTodo todo =
+    div [ class "todo" ]
+        [ text todo.name
+        ]
+
+
+renderAllTodos : List Todo -> Html Msg
+renderAllTodos todos =
+    div [ class "todo-wrapper" ]
+        (List.map renderTodo todos)
 
 
 
@@ -50,7 +91,7 @@ main : Program () Model Msg
 main =
     Browser.element
         { view = view
-        , init = \_ -> init
+        , init = \_ -> ( init, Cmd.none )
         , update = update
         , subscriptions = always Sub.none
         }
